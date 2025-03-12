14e428dc8c025f6df29b0adccbb53239ff5c65dd
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-03-12 01:30:08 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,182 @@
+﻿using System.Numerics;
+using System.Runtime.ExceptionServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////1. Even or Odd
+            //float number;
+            //Console.WriteLine("Enter any Number");
+
+            //number = float.Parse(Console.ReadLine());
+            //if(number % 2 == 0)
+            //{
+            //    Console.WriteLine(number  +"is even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(number + "is odd");
+            //}
+
+            //-------------------------------------
+
+            ////2) Largest of Three Numbers 
+            //int num1,num2,num3;
+            ////double result;
+            //Console.WriteLine("Enter First Number");
+            //num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter seconed Number");
+            //num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter thered  Number");
+            //num3 = int.Parse(Console.ReadLine());
+            //if(num1 >= num2 && num1 >= num3)
+            //{
+            //    Console.WriteLine(num1 + "is largest number");
+            //}
+            //else if(num2 >= num1 && num2 >= num3)
+            //{
+            //    Console.WriteLine(num2+ "is largest number");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(num3 + "is largest number");
+            //}
+
+
+            //------------------------------------------------
+
+            //3. Temperature Converter
+
+            //float temp;
+            //Console.WriteLine("Enter The Temperature in C");
+            //temp = float.Parse(Console.ReadLine());
+            //double result;
+            //result = (temp * 9 / 5) + 32;
+            //Console.WriteLine(result+"F")
+            //;
+
+            //----------------------
+
+            //4. Simple Discount Calculator
+            //double price;
+            //double disc;
+            //double total;
+            //Console.WriteLine("Enter Price");
+            // price = double.Parse(Console.ReadLine());
+            //disc = (price * 10) / 100;
+            //total = price - disc;
+            // if (price > 100)
+            //{
+            //    Console.WriteLine(" Your total price is " + total);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" Your total price is " + price);
+            //}
+
+            //5. Grading System
+            //    float StudentMark;
+
+            //    Console.WriteLine(" Enter your Mark ");
+            //    StudentMark = float.Parse(Console.ReadLine());
+            //    if (StudentMark >= 90 && StudentMark <= 100)
+            //    {
+            //        Console.WriteLine("your grade is A ");
+            //    }
+            //    else if (StudentMark >= 80 && StudentMark <= 89)
+            //        {
+            //        Console.WriteLine("your grade is B ");
+            //    }
+            //    else if (StudentMark >= 70 && StudentMark <= 79)
+            //        {
+            //        Console.WriteLine("your grade is C ");
+            //    }
+
+            //    else if (StudentMark >= 60 && StudentMark <= 69)
+            //    {
+            //        Console.WriteLine("your grade is D ");
+            //    }
+
+            //    else if (StudentMark <= 60)
+            //    {
+            //        Console.WriteLine("your grade is F ");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(" Enter your  correct Mark ");
+            //    }
+
+            //}
+            //------------------------------------------------------
+            //6)Swap Two Numbers
+            //int num1, num2 ;
+            //Console.WriteLine(" Enter first number ");
+            //num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(" Enter second number ");
+            //num2 = int.Parse(Console.ReadLine());
+
+            //int n = num1;
+            //num1 = num2;
+            //num2 = n;
+            //Console.WriteLine(" Numbers After swap ");
+            //Console.WriteLine(" Number1 After swap :" + num1);
+            //Console.WriteLine(" Number2 After swap :" + num2);
+
+            //--------------------------------------
+            //7. Days to Weeks and Days Converter 
+            //float days,week;
+            // Console.WriteLine("enter number of dayes");
+            // days = float.Parse(Console.ReadLine());
+            // week = days / 7;
+            //---------
+
+            //8. Electricity Bill Calculator
+            //float Units;
+            //Console.WriteLine("Enter the Units: \n");
+            //Units = int.Parse(Console.ReadLine());
+            //if (Units < 100)
+            //{
+            //    Console.WriteLine(Units * 0.5);
+            //}
+            //else if (Units < 300)
+            //{
+            //    Console.WriteLine(Units * 0.75);
+            //}
+            //else if (Units > 301)
+            //{
+            //    Console.WriteLine(Units * 1);
+            //}
+
+            //------------------------------------------------
+            //9. Simple Calculator
+
+            float num1,mun2;
+           // string operator;
+            Console.WriteLine(" Enter first number ");
+            num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine(" Enter second  number ");
+            mun2 = float.Parse(Console.ReadLine());
+
+         
+           // operator=Console.ReadLine();
+        
+           
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
